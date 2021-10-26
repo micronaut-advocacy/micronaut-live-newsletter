da269eedb23e0c72a9a761f07a2e643d8466767d
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-26 18:36:35 +0200

    add open api

--- README.md
+++ README.md
@@ -11,3 +11,5 @@
 - [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
 - [Micronaut Management documentation](https://docs.micronaut.io/latest/guide/index.html#management)
 - [Micronaut Problem JSON documentation](https://micronaut-projects.github.io/micronaut-problem-json/latest/guide/index.html)
+- [Micronaut OpenAPI Support documentation](https://micronaut-projects.github.io/micronaut-openapi/latest/guide/index.html)
+- [https://www.openapis.org](https://www.openapis.org)
--- src/main/java/io/micronaut/live/Application.java
+++ src/main/java/io/micronaut/live/Application.java
@@ -1,7 +1,20 @@
 package io.micronaut.live;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Newsletter Subscriber",
+                version = "0.1",
+                description = "Newsletter Subscriber API",
+                license = @License(name = "Apache 2.0"),
+                contact = @Contact(url = "https://sergiodelamo.com", name = "Sergio del Amo")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
