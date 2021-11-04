c20750d20bb77ec7d779ecb08f0684d00d8a8623
Sergio del Amo <sergio.delamo@softamo.com>
2021-11-04 17:38:00 +0100

    Same class implements CodeVerifier and Generator

--- src/main/java/io/micronaut/live/services/JwtConfirmationCodeGenerator.java
+++ src/main/java/io/micronaut/live/services/JwtConfirmationCodeGenerator.java
@@ -1,12 +1,19 @@
 package io.micronaut.live.services;
 
+import com.nimbusds.jwt.JWT;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.security.token.generator.TokenGenerator;
+import io.micronaut.security.token.jwt.encryption.EncryptionConfiguration;
 import io.micronaut.security.token.jwt.generator.claims.JwtClaims;
+import io.micronaut.security.token.jwt.signature.SignatureConfiguration;
+import io.micronaut.security.token.jwt.validator.ExpirationJwtClaimsValidator;
+import io.micronaut.security.token.jwt.validator.JwtValidator;
 import jakarta.inject.Singleton;
 
+import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -14,12 +21,27 @@ import java.util.Map;
 import java.util.Optional;
 
 @Singleton
-public class JwtConfirmationCodeGenerator implements ConfirmationCodeGenerator {
+public class JwtConfirmationCodeGenerator implements ConfirmationCodeGenerator, ConfirmationCodeVerifier {
     public static final String CLAIM_EMAIL = "email";
     private final TokenGenerator tokenGenerator;
+    private final JwtValidator jwtValidator;
 
-    public JwtConfirmationCodeGenerator(TokenGenerator tokenGenerator) {
+    public JwtConfirmationCodeGenerator(TokenGenerator tokenGenerator,
+                                        Collection<SignatureConfiguration> signatureConfigurations,
+                                        Collection<EncryptionConfiguration> encryptionConfigurations,
+                                        ExpirationJwtClaimsValidator expirationJwtClaimsValidator) {
         this.tokenGenerator = tokenGenerator;
+        this.jwtValidator = JwtValidator.builder()
+                .withSignatures(signatureConfigurations)
+                .withEncryptions(encryptionConfigurations)
+                .withClaimValidators(Collections.singletonList(expirationJwtClaimsValidator))
+                .build();
+    }
+
+    @Override
+    public boolean verify(@NonNull @NotBlank String token) {
+        Optional<JWT> optionalJWT = jwtValidator.validate(token, null);
+        return optionalJWT.isPresent();
     }
 
     @Override
