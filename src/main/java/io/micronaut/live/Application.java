04b871831e7f44a70301d5b9f54cc0b54f1cdb77
Sergio del Amo <sergio.delamo@softamo.com>
2021-12-10 17:35:24 +0100

    Add DefaultEnvironmentConfigurer

--- src/main/java/io/micronaut/live/Application.java
+++ src/main/java/io/micronaut/live/Application.java
@@ -1,5 +1,8 @@
 package io.micronaut.live;
 
+import io.micronaut.context.ApplicationContextBuilder;
+import io.micronaut.context.ApplicationContextConfigurer;
+import io.micronaut.context.annotation.ContextConfigurer;
 import io.micronaut.context.env.Environment;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -18,10 +21,21 @@ import io.swagger.v3.oas.annotations.info.License;
 )
 public class Application {
 
+    static final String DEFAULT_ENV = Environment.DEVELOPMENT;
+
+    @ContextConfigurer
+    public static class DefaultEnvironmentConfigurer implements ApplicationContextConfigurer {
+        @Override
+        public void configure(ApplicationContextBuilder context) {
+            context.defaultEnvironments(DEFAULT_ENV);
+        }
+    }
+
+
     public static void main(String[] args) {
         Micronaut.build(args)
                 .mainClass(Application.class)
-                .defaultEnvironments(Environment.DEVELOPMENT)
+                .defaultEnvironments(DEFAULT_ENV)
                 .start();
     }
 }
